c39d7f2c173e35f52763a368eacb15eccb291881
sdubois92 <seb.dubois15@free.fr>
2020-05-14 09:27:48 +0200

    comments

--- routes/comments.js
+++ routes/comments.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const Comment = require('../models/Comment');
 
 
-// Find all users
+// Find all comments
 router.get('/api/comments', (req, res, next) => {
     Comment.find({})
         .then(apiResult => {
@@ -13,28 +13,28 @@ router.get('/api/comments', (req, res, next) => {
         .catch(apiError => res.status(500).json(apiError))
 })
 
-// Find a user in particular
+// Find a comment in particular
 router.get('/api/comments/:id', (req, res, next) => {
     Comment.findById(req.params.id)
         .then(apiResult => res.status(200).json(apiResult))
         .catch(apiError => res.status(500).json(apiError))
 });
 
-// Creating a user
+// Creating a comment
 router.post('/api/comments', (req, res, next) => {
     Comment.create(req.body)
         .then(apiResult => res.status(201).json(apiResult))
         .catch(apiError => res.status(500).json(apiError))
 })
 
-// Updating a user
+// Updating a comment
 router.patch('/api/comments/:id', (req, res, next) => {
     Comment.findByIdAndUpdate(req.params.id, req.body, { new: true })
         .then(apiResult => res.status(200).json(apiResult))
         .catch(apiError => res.status(500).json(apiError))
 });
 
-// Deleting a user
+// Deleting a comment
 router.delete('/api/comments/:id', (req, res, next) => {
     Comment.findByIdAndRemove(req.params.id)
         .then(apiResult => res.status(204).json(apiResult))
